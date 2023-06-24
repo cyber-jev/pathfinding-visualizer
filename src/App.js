c6ae95ed9aed64029c6c90d950ed55bb9af2bc47
Johnny Ekparam <johnnye4u2c@gmail.com>
2023-06-24 06:08:14 +0100

    initialized with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import React from "react";
+import "./App.css";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 
 function App() {
   return (
